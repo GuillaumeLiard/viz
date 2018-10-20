c45ef75e4b906e69c0780c0d865c47a5cc17c1ce
GuillaumeLiard <liardg@gmail.com>
2018-10-20 19:18:46 +0200

    chart

---
+++ components/Chart.vue
@@ -0,0 +1,37 @@
+<template>
+  <svg
+    class="main"
+    version="1.1"
+    baseProfile="full"
+    width="100"
+    height="50"
+    xmlns="http://www.w3.org/2000/svg">
+
+    <rect
+      width="100%"
+      height="100%"
+      fill="red" />
+
+    <circle
+      cx="150"
+      cy="100"
+      r="80"
+      fill="green" />
+
+    <text
+      x="150"
+      y="125"
+      font-size="60"
+      text-anchor="middle"
+      fill="white">
+      SVG2
+    </text>
+  </svg>
+</template>
+
+<style>
+  .main {
+    width: 100%;
+    height: auto;
+  }
+</style>
