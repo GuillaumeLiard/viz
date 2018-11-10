0fa19cea1a8dadc9be8c5cf1534877c847b8d302
GuillaumeLiard <liardg@gmail.com>
2018-11-10 13:47:49 +0100

    BlueBox client Only

---
+++ components/d3/BlueBox.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="view">
+    text
+  </div>
+</template>
+
+<script>
+import * as d3 from 'd3'
+export default {
+  mounted () {
+    // console.log(
+      d3.select('.view')
+        .append('svg')
+        .attr('width', 200)
+        .attr('height', 100)
+        .append('g')
+        .append('rect')
+        .attr('width', 100)
+        .attr('height', 100)
+        .attr('x', 50)
+        .attr('y', 0)
+        .attr('style', 'fill:rgb(0,0,255);')
+      // )
+  }
+}
+</script>
---
+++ pages/d3/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <section class="container">
+    <div class="content">
+      <D3component/>
+    </div>
+  </section>
+</template>
+
+<script>
+import D3component from '~/components/d3/BlueBox.vue'
+
+export default {
+  components: {
+    D3component
+  },
+  data: function() {
+    return {
+      points: [
+        [0, 10],
+        [1, 5],
+        [2, 9]
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import '~assets/variables.scss';
+
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.content {
+  width: $viz-width;
+}
+</style>
